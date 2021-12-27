5faf7211ba463334e3ba31e2de10b3ac45912b70
Labbouyat <labbouyat@gmail.com>
2021-12-27 12:37:46 -0500

    Exercise Completed

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 17;
+            int b = 4;
+            int d = a / b;
+            int e = a % b;
+            if (a ==17 && b ==4)
+            Console.WriteLine($"quotient: {d}");
+            Console.WriteLine($"remainder: {e}");
+        }
+    }
+}
